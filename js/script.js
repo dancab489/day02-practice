31ca5e19986670fe9e5d92aced753b9d9c67b6e3
Dan Cab <dancab489@gmail.com>
2022-08-20 18:24:30 -0700

    daily challenge

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const humantime = {person:"alive", wannabe:"unalive"}
+console.log(humantime);
+let werd = `Supercalifragilisticexpialidocious`;
+let lenf = werd.length;
+console.log(lenf);
+const phattyBase = BigInt(9n);
+const phattyExp = BigInt(50n);
+const phattyNum = BigInt(phattyBase**phattyExp);
+const biggie ={num:phattyNum, type:typeof phattyNum};
+console.log(biggie);
+const val = 7;
+const lil = {whatValue: val, whatKind: typeof val};
+console.log(lil);
